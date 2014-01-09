6e6a5d67dd085083303d053ea73cf8a46383877a
pjambet <pierre.jambet@gmail.com>
2014-01-09 03:25:17 +0100

    feat(SegmentFetcher): Fetch activities from moves

--- app/services/segment_fetcher.rb
+++ app/services/segment_fetcher.rb
@@ -1,3 +1,15 @@
 class SegmentFetcher
+  attr_reader :user
   # Fetch all segments from moves api and locate them
+  def initialize(user)
+    @user = user
+  end
+
+  def fetch(date=nil)
+    client.daily_activities date
+  end
+
+  def client
+    Moves::Client.new(user.token)
+  end
 end
--- spec/services/segment_fetcher_spec.rb
+++ spec/services/segment_fetcher_spec.rb
@@ -1,4 +1,24 @@
 require 'spec_helper'
 
 describe SegmentFetcher do
+  context 'new instance' do
+    let(:user) { instance_double('User') }
+    subject(:fetcher) { SegmentFetcher.new(user) }
+
+    it { expect(fetcher).not_to be_nil }
+  end
+
+  describe '#fetch' do
+    let(:user) { mock_model('User', token: '5t49q6Tpe8nCNEQ7bJxchcRJ4oQU25ebOumrWTe1RTqmEivJvrJjOcu2w5kMeFms') }
+    subject(:fetcher) { SegmentFetcher.new(user) }
+
+    context 'specific date' do
+      let(:result) do
+        VCR.use_cassette('SegmentFetcher/fetch_specific') { fetcher.fetch '2013-01-01' }
+      end
+
+      it { expect(result).to be_an_instance_of(Array) }
+      it { expect(result[0]['date']).to eq('20130101') }
+    end
+  end
 end
