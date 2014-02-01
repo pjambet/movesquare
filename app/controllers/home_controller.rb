31cd730bbf960ccc9c645b53a9b85c12b3458234
pjambet <pierre.jambet@gmail.com>
2014-02-01 18:57:00 -0500

    fix(HomeController): Prevent error when no one is logged in

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,5 @@
 class HomeController < ApplicationController
   def index
-    @profile = Moves::Client.new(current_user.token).profile
+    @profile = Moves::Client.new(current_user.token).profile if current_user
   end
 end
