7a184e8b9f4ec1b6dcf00d620f5f4631d1b4a256
pjambet <pierre.jambet@gmail.com>
2014-02-04 00:05:25 -0500

    chore(schema.rb): Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,6 @@ ActiveRecord::Schema.define(version: 20140102133523) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-  enable_extension "hstore"
 
   create_table "locations", force: true do |t|
     t.float    "lat"
